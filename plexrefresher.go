24fea67d165e17485b518b3cb152677a3c8b4dea
Jordan Potter <me@jordanpotter.com>
2019-06-13 09:35:32 -0700

    Check for empty endpoint

--- plexrefresher.go
+++ plexrefresher.go
@@ -14,7 +14,9 @@ type PlexRefresher struct {
 }
 
 func New(endpoint, token string) (*PlexRefresher, error) {
-	if _, err := url.Parse(endpoint); err != nil {
+	if endpoint == "" {
+		return nil, errors.New("endpoint cannot be empty")
+	} else if _, err := url.Parse(endpoint); err != nil {
 		return nil, errors.Wrap(err, "failed to parse endpoint")
 	}
 
